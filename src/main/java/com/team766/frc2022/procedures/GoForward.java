e94510939fe122e065ef096983cb4544dd8f07a5
frenchyalex <apocesar11@gmail.com>
2022-07-20 20:50:21 -0700

    Attempt at 2d pid conttroler
    1st attempt at using a pid controller for swerve drive autonomous with adrian's odometry

---
+++ src/main/java/com/team766/frc2022/procedures/GoForward.java
@@ -0,0 +1,27 @@
+package com.team766.frc2022.procedures;
+
+import com.team766.framework.Context;
+import com.team766.framework.Procedure;
+import com.team766.frc2022.Robot;
+import com.team766.logging.Category;
+import com.team766.controllers.PIDController;
+
+public class GoForward extends Procedure {
+
+	public void run(Context context) {
+		context.takeOwnership(Robot.drive);
+		context.takeOwnership(Robot.gyro);
+		loggerCategory = Category.AUTONOMOUS;
+		double x = Robot.drive.getCurrentPosition().getX();
+		double y = Robot.drive.getCurrentPosition().getY();
+		PIDController p_pid = new PIDController(1, 0, 0, -1, 1, 0.05);
+		p_pid.setSetpoint(1);
+		while(!p_pid.isDone()){
+			x = Robot.drive.getCurrentPosition().getX();
+			p_pid.calculate(x, true);
+			Robot.drive.setGyro(Robot.gyro.getGyroYaw());;
+			Robot.drive.swerveDrive(p_pid.getOutput(), 0, 0);
+			log("Error: " + p_pid.getCurrentError());
+		}
+	}
+}
